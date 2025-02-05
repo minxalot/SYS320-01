f4deedf93df7e05aa70d396ee0bfa78252d35189
unknown <lindsey>
2025-02-05 14:11:50 -0500

    classwork for week 4

---
+++ week4/Deliverables2-8.ps1
@@ -0,0 +1,15 @@
+﻿$notfounds = Get-Content -Path C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+$regex = [regex] "\b(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\b"
+
+$ipsUnorganized = $regex.Matches($notfounds)
+
+$ips = @()
+
+for($i=0; $i -lt $ipsUnorganized.Count; $i++) {
+    $ips += [PSCustomObject]@{ "IP" = $ipsUnorganized[$i].Value; }
+}
+
+$ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+$counts = $ipsoftens | Group-Object IP
+$counts | Select-Object Count, Name
