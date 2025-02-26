3723c65db7c1ed1135d0535f412095dab340d87a
unknown <lindsey>
2025-02-26 00:04:21 -0500

    in-class work for week 6

--- week6/main.ps1
+++ week6/main.ps1
@@ -1,7 +1,8 @@
-﻿ . (Join-Path $PSScriptRoot Users.ps1)
+﻿. (Join-Path $PSScriptRoot Users.ps1)
 . (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot String-Helper.ps1)
 
-clear
+# clear
 
 $Prompt = "`n"
 $Prompt += "Please choose your operation:`n"
@@ -13,141 +14,141 @@ $Prompt += "5 - Enable a User`n"
 $Prompt += "6 - Disable a User`n"
 $Prompt += "7 - Get Log-In Logs`n"
 $Prompt += "8 - Get Failed Log-In Logs`n"
-$Prompt += "9 - Exit`n"
-
-
+$Prompt += "9 - List At Risk Users`n"
+$Prompt += "10 - Exit`n"
 
 $operation = $true
 
-while($operation){
+function checkUser($name) {
+    $userExists = Get-LocalUser | Where-Object { $_.Name -ilike $name }
+    return $userExists
+}
 
-    
+function checkPassword($password) {
+
+    if ($password.Length -lt 6) {
+        Write-Host "Password must be at least 6 characters long."
+        return $false
+    }
+    if ($password -notmatch '\d') {
+        Write-Host "Password must contain at least 1 number."
+        return $false
+    }
+    if ($password -notmatch '[A-Za-z]') {
+        Write-Host "Password must contain at least 1 letter."
+        return $false
+    }
+    if ($password -notmatch '[!@#$%^&*(),.?":{}|<>]') {
+        Write-Host "Password must contain at least 1 special character."
+        return $false
+    }
+    return $true
+}
+
+while ($operation) {
     Write-Host $Prompt | Out-String
-    $choice = Read-Host 
+    $choice = Read-Host
 
-
-    if($choice -eq 9){
+    if ($choice -eq 10) {
         Write-Host "Goodbye" | Out-String
         exit
-        $operation = $false 
+        $operation = $false
     }
-
-    elseif($choice -eq 1){
+    elseif ($choice -eq 1) {
         $enabledUsers = getEnabledUsers
         Write-Host ($enabledUsers | Format-Table | Out-String)
     }
-
-    elseif($choice -eq 2){
+    elseif ($choice -eq 2) {
         $notEnabledUsers = getNotEnabledUsers
         Write-Host ($notEnabledUsers | Format-Table | Out-String)
     }
-
-
-    # Create a user
-    elseif($choice -eq 3){ 
-
+    elseif ($choice -eq 3) {
         $name = Read-Host -Prompt "Please enter the username for the new user"
-        $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+        $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user:"
+        
+        # Check if the user already exists
+        if (checkUser $name) {
+            Write-Host "User already exists. Please choose a different username." | Out-String
+            continue
+        }
 
-        # TODO: Create a function called checkUser in Users that: 
-        #              - Checks if user a exists. 
-        #              - If user exists, returns true, else returns false
-        # TODO: Check the given username with your new function.
-        #              - If false is returned, continue with the rest of the function
-        #              - If true is returned, do not continue and inform the user
-        #
-        # TODO: Create a function called checkPassword in String-Helper that:
-        #              - Checks if the given string is at least 6 characters
-        #              - Checks if the given string contains at least 1 special character, 1 number, and 1 letter
-        #              - If the given string does not satisfy conditions, returns false
-        #              - If the given string satisfy the conditions, returns true
-        # TODO: Check the given password with your new function. 
-        #              - If false is returned, do not continue and inform the user
-        #              - If true is returned, continue with the rest of the function
+        if (-not (checkPassword $password)) {
+            continue
+        }
 
         createAUser $name $password
-
         Write-Host "User: $name is created." | Out-String
     }
-
-
-    # Remove a user
-    elseif($choice -eq 4){
-
+    elseif ($choice -eq 4) {
         $name = Read-Host -Prompt "Please enter the username for the user to be removed"
 
-        # TODO: Check the given username with the checkUser function.
+        if (-not (checkUser $name)) {
+            Write-Host "User does not exist." | Out-String
+            continue
+        }
 
         removeAUser $name
-
         Write-Host "User: $name Removed." | Out-String
     }
-
-
-    # Enable a user
-    elseif($choice -eq 5){
-
-
+    elseif ($choice -eq 5) {
         $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
 
-        # TODO: Check the given username with the checkUser function.
+        if (-not (checkUser $name)) {
+            Write-Host "User does not exist." | Out-String
+            continue
+        }
 
         enableAUser $name
-
         Write-Host "User: $name Enabled." | Out-String
     }
-
-
-    # Disable a user
-    elseif($choice -eq 6){
-
+    elseif ($choice -eq 6) {
         $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
 
-        # TODO: Check the given username with the checkUser function.
+        if (-not (checkUser $name)) {
+            Write-Host "User does not exist." | Out-String
+            continue
+        }
 
         disableAUser $name
-
         Write-Host "User: $name Disabled." | Out-String
     }
-
-
-    elseif($choice -eq 7){
-
+    elseif ($choice -eq 7) {
         $name = Read-Host -Prompt "Please enter the username for the user logs"
 
-        # TODO: Check the given username with the checkUser function.
+        if (-not (checkUser $name)) {
+            Write-Host "User does not exist." | Out-String
+            continue
+        }
 
-        $userLogins = getLogInAndOffs 90
-        # TODO: Change the above line in a way that, the days 90 should be taken from the user
-
-        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+        $days = Read-Host -Prompt "Please enter the number of days for log retrieval"
+        $userLogins = getLogInAndOffs $days
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name" } | Format-Table | Out-String)
     }
-
-
-    elseif($choice -eq 8){
-
+    elseif ($choice -eq 8) {
         $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
 
-        # TODO: Check the given username with the checkUser function.
+        if (-not (checkUser $name)) {
+            Write-Host "User does not exist." | Out-String
+            continue
+        }
 
-        $userLogins = getFailedLogins 90
-        # TODO: Change the above line in a way that, the days 90 should be taken from the user
-
-        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+        $days = Read-Host -Prompt "Please enter the number of days for failed logins"
+        $userLogins = getFailedLogins $days
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name" } | Format-Table | Out-String)
     }
+    elseif ($choice -eq 9) {
+        $days = Read-Host -Prompt "Please enter the number of days for checking failed logins"
+        $failedLogins = getFailedLogins $days
+        $usersAtRisk = $failedLogins | Group-Object User | Where-Object { $_.Count -gt 10 }
 
-
-    # TODO: Create another choice "List at Risk Users" that
-    #              - Lists all the users with more than 10 failed logins in the last <User Given> days.  
-    #                (You might need to create some failed logins to test)
-    #              - Do not forget to update prompt and option numbers
-    
-    # TODO: If user enters anything other than listed choices, e.g. a number that is not in the menu   
-    #       or a character that should not be accepted. Give a proper message to the user and prompt again.
-    
-
+        if ($usersAtRisk) {
+            Write-Host "Users at risk:" | Out-String
+            Write-Host ($usersAtRisk | Format-Table | Out-String)
+        } else {
+            Write-Host "No users at risk." | Out-String
+        }
+    }
+    else {
+        Write-Host "Womp womp. You messed up. Try again." | Out-String
+    }
 }
-
-
-
-
