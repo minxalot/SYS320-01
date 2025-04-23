269f2e46854a9dfb3a40e14ccddb0fc91ae37671
Lindsey Bellaire <lindsey,.bellaire@mymail.champlain.edu>
2025-04-23 12:34:11 -0400

    oops

--- week14/readingtables.bash
+++ week14/readingtables.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-pageContent=$(curl -s http://
+pageContent=$(curl -s "10.0.17.6/Assignment.html")
+
+times=$(
+echo "$pageContent" | \
+xmlstarlet select --html --recover --template --match '(//table)[1]/tr[position()>1]' --value-of 'td[2]
+)
+
+pressures=$(
+)
+
+temps=$(
+echo "$pageContent" | \
+xmlstarlet select --html --recover --template --match '(//table)[1]/tr[position()>1]' --value-of 'td[1]' --nl
+)
