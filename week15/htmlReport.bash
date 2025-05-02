0d653b934245e7e82fb057c64a91f17bae82c63c
Lindsey Bellaire <lindsey,.bellaire@mymail.champlain.edu>
2025-05-01 23:37:12 -0400

    final is done!

--- week15/htmlReport.bash
+++ week15/htmlReport.bash
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+input="report.txt"
+output="/var/www/html/report.html"
+
+{
+	echo "<!DOCTYPE html>"
+	echo "<html>"
+	echo "<head>"
+		echo "<title>IOC Report</title>"
+		echo "<style>th, td { border: 1px solid #000000; }</style>"
+	echo "</head>"
+	echo "<body>"
+	echo "<table>"
+} > "$output"
+
+{
+	while read -r ip timestamp page; do
+		echo "<tr>"
+		echo "<td>$ip</td>"
+		echo "<td>$timestamp</td>"
+		echo "<td>$page</td>"
+	done < report.txt
+} >> "$output"
+
+{
+	echo "</table>"
+	echo "</body>"
+	echo "</html>"
+} >> "$output"
