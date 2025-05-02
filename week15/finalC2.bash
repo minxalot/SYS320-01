81cf511d2a1c0204adfdafca14f6eb4ccad2b858
Lindsey Bellaire <lindsey,.bellaire@mymail.champlain.edu>
2025-05-01 23:02:56 -0400

    finalC2

--- week15/finalC2.bash
+++ week15/finalC2.bash
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+> "report.txt"
+
+logFile="$1"
+iocFile="$2"
+
+while read -r logLine; do
+	while read -r ioc; do
+		if echo "$logLine" | grep -q "$ioc"; then
+			ip=$(echo "$logLine" | cut -d' ' -f1)
+			timestamp=$(echo "$logLine" | cut -d' ' -f4 | tr -d '[]')
+			page=$(echo "logLine" | cut -d' ' -f7)
+			echo "$ip $timestamp $page" >> report.txt
+		fi
+	done < "iocFile"
+done < "logFile"
